5805d6c4c4381aa34ebd4a7eceabad3c6a80913f
Shivam Sayankar <111188660+Shivam-Sayankar@users.noreply.github.com>
2026-05-05 18:00:29 +0530

    feat: add AI-powered diff explanation feature     - implement explainChanges command using Gemini API     - generate concise explanations of staged changes     - reuse getStagedDiff for consistent git data access     - standardize AI usage with gemini-flash-latest model

--- extension.js
+++ extension.js
@@ -12,13 +12,29 @@ const gemini = new GoogleGenAI({
 	apiKey: process.env.GEMINI_API_KEY
 })
 
+function getStagedDiff(workspaceFolder) {
+	return new Promise((resolve, reject) => {
+		exec('git diff --cached', { cwd: workspaceFolder }, (err, stdout, stderr) => {
+			if (err || !stdout) {
+				reject('No staged changes found')
+			}
+			else {
+				resolve(stdout)
+			}
+		})
+	})
+}
+
+function getRecentCommits() { }
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 	console.log('Extension "GitWiser" is now active!');
 
-	let disposable = vscode.commands.registerCommand('gitwiser.generateCommit', async function () {
+	// Generate Commit Message
+	let generateCommitMessage = vscode.commands.registerCommand('gitwiser.generateCommit', async function () {
 
 		const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
 
@@ -27,57 +43,107 @@ function activate(context) {
 			return
 		}
 
+		try {
 
-		exec('git diff --cached', { cwd: workspaceFolder }, async (err, stdout, stderr) => {
-			if (err || !stdout) {
-				vscode.window.showErrorMessage('No staged changes found')
-				return
-			}
+			const diff = await getStagedDiff(workspaceFolder)
 
 			const prompt = `You are an expert developer, who has been a guide to countless students.
 							For the given git diff, generate a concise and meaningful commit message.
 							Follow conventional commit formats if possible
 							Git diff:
-							${stdout}`
+							${diff}`
 
-			try {
+			const response = await gemini.models.generateContent({
+				model: "gemini-flash-latest",
+				contents: prompt
+			})
 
-				const response = await gemini.models.generateContent({
-					model: "gemini-3-flash-preview",
-					contents: prompt
-				})
+			const commitMessage = response.text
 
-				const message = response.text
-
-				if (!message) {
-					vscode.window.showErrorMessage('Failed to generate commit message')
-					return
-				}
-
-				const action = await vscode.window.showInformationMessage(
-					"GitWiser: Commit message generated",
-					"Copy",
-					"Show"
-				)
-
-				if (action === "Copy") {
-					await vscode.env.clipboard.writeText(message)
-					vscode.window.showInformationMessage('Copied to clipboard')
-				}
-				else if (action === "Show") {
-					vscode.window.showInformationMessage(message)
-				}
+			if (!commitMessage) {
+				vscode.window.showErrorMessage('Failed to generate commit message')
+				return
 			}
 
-			catch (error) {
-				console.log("Error: ", error)
-				vscode.window.showErrorMessage('AI request failed.')
-			}
+			const action = await vscode.window.showInformationMessage(
+				"GitWiser: Commit message generated",
+				"Copy",
+				"Show"
+			)
 
-		})
+			if (action === "Copy") {
+				await vscode.env.clipboard.writeText(commitMessage)
+				vscode.window.showInformationMessage('Copied to clipboard')
+			}
+			else if (action === "Show") {
+				vscode.window.showInformationMessage(commitMessage)
+			}
+		}
+
+		catch (error) {
+			console.log("Error: ", error)
+			vscode.window.showErrorMessage('AI request failed.')
+		}
 	})
 
-	context.subscriptions.push(disposable);
+	let explainGitDiff = vscode.commands.registerCommand('gitwiser.explainDiffChanges', async function () {
+
+		const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+
+		if (!workspaceFolder) {
+			vscode.window.showErrorMessage('NO workspace folder found,')
+			return
+		}
+
+		try {
+			const diff = await getStagedDiff(workspaceFolder)
+
+			const prompt = `You are an experienced developer and guide to many developers/students
+Explain the following git diff in simple and clear English.
+Focus on:
+- What changes were made
+- Why they might have been made
+- Keep it concise
+
+Git diff:
+${diff}`
+
+			const response = await gemini.models.generateContent({
+				model: "gemini-flash-latest",
+				contents: prompt
+			})
+
+			const explanation = response.text
+
+			if (!explanation) {
+				vscode.window.showErrorMessage('Failed to generate Explanation')
+				return
+			}
+
+			const action = await vscode.window.showInformationMessage(
+				"Gitwiser: Explanation generated",
+				"Show",
+				"Copy"
+			)
+
+			if (action === "Show") {
+				vscode.window.showInformationMessage(explanation)
+			}
+			else if (action === 'Copy') {
+				await vscode.env.clipboard.writeText(explanation)
+				vscode.window.showInformationMessage('Explanation Copied to Clipboard')
+			}
+		}
+
+		catch (error) {
+			console.log("Error: ", error)
+			vscode.window.showErrorMessage('AI request failed.')
+		}
+
+	})
+
+	context.subscriptions.push(generateCommitMessage);
+	context.subscriptions.push(explainGitDiff);
 }
 
 function deactivate() { }
